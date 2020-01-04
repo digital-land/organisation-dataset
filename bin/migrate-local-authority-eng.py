c8ba2e5648b94528cbed0180182fde50183fbda7
Paul Downey <psd@whatfettle.com>
2020-01-04 17:53:54 +0000

    Black

--- bin/migrate-local-authority-eng.py
+++ bin/migrate-local-authority-eng.py
@@ -5,19 +5,19 @@ import csv
 
 fieldnames = [
     "local-authority-eng",
-
     # for matching and patching register data
     "wikidata",
     "statistical-geography",
     "esd-inventories",
-
     # data missing from the register
     "name",
     "local-authority-type",
     "start-date",
 ]
 
-writer = csv.DictWriter(open('local-authority-eng.csv', 'w'), fieldnames=fieldnames, extrasaction="ignore")
+writer = csv.DictWriter(
+    open("local-authority-eng.csv", "w"), fieldnames=fieldnames, extrasaction="ignore"
+)
 writer.writeheader()
 
 for row in csv.DictReader(open("data/local-authority-eng.csv")):
